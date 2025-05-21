311be7ab88275d1ca8b3bc0bae8c21e3ad5d3180
Kris Warner <kris@kdwarn.net>
2025-05-21 10:19:12 -0400

    Improve comments; use consistent case

--- src/pa/2023/populate_data_tables.sql
+++ src/pa/2023/populate_data_tables.sql
@@ -30,32 +30,32 @@ begin
     end loop;
 
     -- Alter some specific text field values, usually from lookup tables.
-    execute format($query$update temp_cycle set mc_dvr_hlmt_type = Null where mc_dvr_hlmt_type = ' '$query$);
-    execute format($query$update temp_cycle set mc_pas_hlmt_type = Null where mc_pas_hlmt_type = ' '$query$);
-    execute format($query$update temp_person set extric_ind = Null where extric_ind = '9'$query$);
-    execute format($query$update temp_person set restraint_helmet = Null where restraint_helmet in ('13', '14')$query$);
-    execute format($query$update temp_person set seat_position = Null where seat_position in ('16', '17', '18', '19', '20')$query$);
-    execute format($query$update temp_roadway set rdwy_orient = Null where rdwy_orient = 'B'$query$);
-    execute format($query$update temp_trailveh set trl_veh_reg_state = Null where trl_veh_reg_state = '0'$query$);
-    execute format($query$update temp_vehicle set avoid_man_cd = Null where avoid_man_cd = '9'$query$);
-    execute format($query$update temp_vehicle set make_cd = Null where make_cd in ('KALM', 'KNNW')$query$);
-    execute format($query$update temp_vehicle set veh_position = Null where veh_position = '00'$query$);
-    execute format($query$update temp_vehicle set vina_body_type_cd = Null where vina_body_type_cd in ('T', 'P4D', 'P', 'PSW', 'P4H', 'C', 'M', 'P3P', 'T2W', 'PC4')$query$);
+    execute format($query$update temp_cycle set mc_dvr_hlmt_type = null where mc_dvr_hlmt_type = ' '$query$);
+    execute format($query$update temp_cycle set mc_pas_hlmt_type = null where mc_pas_hlmt_type = ' '$query$);
+    execute format($query$update temp_person set extric_ind = null where extric_ind = '9'$query$);
+    execute format($query$update temp_person set restraint_helmet = null where restraint_helmet in ('13', '14')$query$);
+    execute format($query$update temp_person set seat_position = null where seat_position in ('16', '17', '18', '19', '20')$query$);
+    execute format($query$update temp_roadway set rdwy_orient = null where rdwy_orient = 'B'$query$);
+    execute format($query$update temp_trailveh set trl_veh_reg_state = null where trl_veh_reg_state = '0'$query$);
+    execute format($query$update temp_vehicle set avoid_man_cd = null where avoid_man_cd = '9'$query$);
+    execute format($query$update temp_vehicle set make_cd = null where make_cd in ('KALM', 'KNNW')$query$);
+    execute format($query$update temp_vehicle set veh_position = null where veh_position = '00'$query$);
+    execute format($query$update temp_vehicle set vina_body_type_cd = null where vina_body_type_cd in ('T', 'P4D', 'P', 'PSW', 'P4H', 'C', 'M', 'P3P', 'T2W', 'PC4')$query$);
     
-    -- Alter the values of the fields that are supposed to be booleans - e.g. set 'U' and
-    -- ints higher than 1 to null.
+    -- Alter the values of the fields in the temp tables that will end up being booleans - e.g.
+    -- set 'U' and ints higher than 1 to null.
     foreach db_table in array db_tables loop
         for col_name in select column_name from information_schema.Columns where table_name = db_table and table_schema = 'pa_2023' and data_type = 'boolean' loop
-            execute format($query$update temp_%I set %I = Null where %I = 'U'$query$, db_table, col_name, col_name);
-            execute format($query$update temp_%I set %I = Null where %I = ' '$query$, db_table, col_name, col_name);
+            execute format($query$update temp_%I set %I = null where %I = 'U'$query$, db_table, col_name, col_name);
+            execute format($query$update temp_%I set %I = null where %I = ' '$query$, db_table, col_name, col_name);
             execute format($query$update temp_%I set %I = '1' where %I = '01'$query$, db_table, col_name, col_name);
             execute format($query$update temp_%I set %I = '0' where %I = '00'$query$, db_table, col_name, col_name);
             -- Must wrap in an exception since the cast could fail.
             begin
-                execute format($query$update temp_%I set %I = Null where cast(%I as int) > 1$query$, db_table, col_name, col_name);
+                execute format($query$update temp_%I set %I = null where cast(%I as int) > 1$query$, db_table, col_name, col_name);
             exception
                 when invalid_text_representation then
-                    NULL; -- No need to do anything here - if it can't be cast as int, that's fine. 
+                    null; -- No need to do anything here - if it can't be cast as int, that's fine. 
                 when others then
                     -- If there are other errors, raise notice showing code.
                     raise notice '%', SQLSTATE;
@@ -63,7 +63,7 @@ begin
         end loop;
 
     -- Another boolean change, but which seems unique to this field and possibly meant to be something else.
-    execute format($query$update temp_person set transported = Null where transported = 'R'$query$);
+    execute format($query$update temp_person set transported = null where transported = 'R'$query$);
 
     end loop;
 
