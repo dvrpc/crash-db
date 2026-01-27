fe77a02ed0caf695a93781c447d07ca1487eb481
brcarney <44352367+brcarney@users.noreply.github.com>
2026-01-27 11:00:45 -0500

    refactor: deleted redundant files and renamed existing

--- pa_table_summary/sql/nj_report_long.sql
+++ pa_table_summary/sql/nj_report_long.sql
@@ -5,44 +5,77 @@ create view nj.report_summary_long as
 /* =========================================================
    COLLISION TYPE
    ========================================================= */
-select c.casenumber, 'collision_type' as domain, 'noncollision' as category, 1 as cnt
-from nj.all_crash c where c.collision_type = '0'
+select c.casenumber, 'collision_type' as domain, 'unknown' as category, 1 as cnt
+from nj.all_crash c where c.crash_type = '00'
 
 union all
-select c.casenumber, 'collision_type', 'rearend', 1
-from nj.all_crash c where c.collision_type = '1'
+select c.casenumber, 'collision_type', 'same_direction_rearend', 1
+from nj.all_crash c where c.crash_type = '01'
 
 union all
-select c.casenumber, 'collision_type', 'headon', 1
-from nj.all_crash c where c.collision_type = '2'
+select c.casenumber, 'collision_type', 'same_direction_sideswipe', 1
+from nj.all_crash c where c.crash_type = '02'
+
+union all
+select c.casenumber, 'collision_type', 'right_angle', 1
+from nj.all_crash c where c.crash_type = '03'
+
+union all
+select c.casenumber, 'collision_type', 'opposite_direction_headon_angular', 1
+from nj.all_crash c where c.crash_type = '04'
+
+union all
+select c.casenumber, 'collision_type', 'opposite_direction_sideswipe', 1
+from nj.all_crash c where c.crash_type = '05'
+
+union all
+select c.casenumber, 'collision_type', 'struck_parked_vehicle', 1
+from nj.all_crash c where c.crash_type = '06'
+
+union all
+select c.casenumber, 'collision_type', 'left_turn_u_turn', 1
+from nj.all_crash c where c.crash_type = '07'
 
 union all
 select c.casenumber, 'collision_type', 'backing', 1
-from nj.all_crash c where c.collision_type = '3'
+from nj.all_crash c where c.crash_type = '08'
 
 union all
-select c.casenumber, 'collision_type', 'angle', 1
-from nj.all_crash c where c.collision_type = '4'
+select c.casenumber, 'collision_type', 'encroachment', 1
+from nj.all_crash c where c.crash_type = '09'
 
 union all
-select c.casenumber, 'collision_type', 'sideswipe_same', 1
-from nj.all_crash c where c.collision_type = '5'
+select c.casenumber, 'collision_type', 'overturned', 1
+from nj.all_crash c where c.crash_type = '10'
 
 union all
-select c.casenumber, 'collision_type', 'sideswipe_opposite', 1
-from nj.all_crash c where c.collision_type = '6'
+select c.casenumber, 'collision_type', 'fixed_object', 1
+from nj.all_crash c where c.crash_type = '11'
 
 union all
-select c.casenumber, 'collision_type', 'hit_fixed_object', 1
-from nj.all_crash c where c.collision_type = '7'
+select c.casenumber, 'collision_type', 'animal', 1
+from nj.all_crash c where c.crash_type = '12'
 
 union all
-select c.casenumber, 'collision_type', 'hit_nonmotorist', 1
-from nj.all_crash c where c.collision_type = '8'
+select c.casenumber, 'collision_type', 'pedestrian', 1
+from nj.all_crash c where c.crash_type = '13'
 
 union all
-select c.casenumber, 'collision_type', 'other_unknown', 1
-from nj.all_crash c where c.collision_type in ('9','98','99')
+select c.casenumber, 'collision_type', 'pedalcyclist', 1
+from nj.all_crash c where c.crash_type = '14'
+
+union all
+select c.casenumber, 'collision_type', 'non_fixed_object', 1
+from nj.all_crash c where c.crash_type = '15'
+
+union all
+select c.casenumber, 'collision_type', 'railcar_vehicle', 1
+from nj.all_crash c where c.crash_type = '16'
+
+
+union all
+select c.casenumber, 'collision_type', 'other', 1
+from nj.all_crash c where c.crash_type = '99'
 
 
 /* =========================================================
@@ -50,118 +83,108 @@ from nj.all_crash c where c.collision_type in ('9','98','99')
    ========================================================= */
 union all
 select c.casenumber, 'severity', 'no_injury', 1
-from nj.all_crash c where c.max_severity_level = '0'
+from nj.all_crash c where c.severity = 'P'
 
 union all
 select c.casenumber, 'severity', 'fatal', 1
-from nj.all_crash c where c.max_severity_level = '1'
+from nj.all_crash c where c.severity = 'F'
 
 union all
-select c.casenumber, 'severity', 'serious', 1
-from nj.all_crash c where c.max_severity_level = '2'
-
-union all
-select c.casenumber, 'severity', 'minor', 1
-from nj.all_crash c where c.max_severity_level = '3'
-
-union all
-select c.casenumber, 'severity', 'possible', 1
-from nj.all_crash c where c.max_severity_level = '4'
-
-union all
-select c.casenumber, 'severity', 'injury_unknown', 1
-from nj.all_crash c where c.max_severity_level in ('8','9')
+select c.casenumber, 'severity', 'injury', 1
+from nj.all_crash c where c.severity = 'I'
 
 
 /* =========================================================
    ROAD CONDITION
    ========================================================= */
 union all
+select c.casenumber, 'road_condition', 'unknown', 1
+from nj.all_crash c where c.road_surface_condition = '00'
+
+union all
 select c.casenumber, 'road_condition', 'dry', 1
-from nj.all_crash c where c.road_condition = '01'
-
-union all
-select c.casenumber, 'road_condition', 'ice', 1
-from nj.all_crash c where c.road_condition = '02'
-
-union all
-select c.casenumber, 'road_condition', 'snow', 1
-from nj.all_crash c where c.road_condition = '07'
-
-union all
-select c.casenumber, 'road_condition', 'water', 1
-from nj.all_crash c where c.road_condition = '08'
+from nj.all_crash c where c.road_surface_condition = '01'
 
 union all
 select c.casenumber, 'road_condition', 'wet', 1
-from nj.all_crash c where c.road_condition = '09'
+from nj.all_crash c where c.road_surface_condition = '02'
+
+union all
+select c.casenumber, 'road_condition', 'snow', 1
+from nj.all_crash c where c.road_surface_condition = '03'
+
+union all
+select c.casenumber, 'road_condition', 'icy', 1
+from nj.all_crash c where c.road_surface_condition = '04'
 
 union all
 select c.casenumber, 'road_condition', 'other', 1
-from nj.all_crash c
-where c.road_condition in ('03','04','05','06','22','98')
-
-union all
-select c.casenumber, 'road_condition', 'unknown', 1
-from nj.all_crash c where c.road_condition = '99'
+from nj.all_crash c where c.road_surface_condition in ('05', '06', '07', '08', '09', '99')
 
 
 /* =========================================================
    WEATHER
    ========================================================= */
 union all
+select c.casenumber, 'weather', 'unknown', 1
+from nj.all_crash c where c.environmental_condition = '00'
+
+union all
 select c.casenumber, 'weather', 'clear', 1
-from nj.all_crash c where c.weather1 = '03'
-
-union all
-select c.casenumber, 'weather', 'cloudy', 1
-from nj.all_crash c where c.weather1 = '04'
-
-union all
-select c.casenumber, 'weather', 'fog_smog_smoke', 1
-from nj.all_crash c where c.weather1 = '05'
+from nj.all_crash c where c.environmental_condition = '01'
 
 union all
 select c.casenumber, 'weather', 'rain', 1
-from nj.all_crash c where c.weather1 = '07'
+from nj.all_crash c where c.environmental_condition = '02'
 
 union all
 select c.casenumber, 'weather', 'snow', 1
-from nj.all_crash c where c.weather1 = '10'
+from nj.all_crash c where c.environmental_condition = '03'
 
 union all
-select c.casenumber, 'weather', 'other', 1
-from nj.all_crash c
-where c.weather1 in ('01','02','06','08','09','98')
+select c.casenumber, 'weather', 'clear', 1
+from nj.all_crash c where c.environmental_condition in ('04', '05', '06', '07', '08', '09', '10', '99')
 
-union all
-select c.casenumber, 'weather', 'unknown', 1
-from nj.all_crash c where c.weather1 = '99'
 
 
 /* =========================================================
    ILLUMINATION
    ========================================================= */
 union all
+select c.casenumber, 'illumination', 'unknown', 1
+from nj.all_crash c where c.light_condition = '00'
+
+union all
 select c.casenumber, 'illumination', 'daylight', 1
-from nj.all_crash c where c.illumination = '1'
+from nj.all_crash c where c.light_condition = '01'
 
 union all
-select c.casenumber, 'illumination', 'dark_no_street', 1
-from nj.all_crash c where c.illumination = '2'
+select c.casenumber, 'illumination', 'dawn', 1
+from nj.all_crash c where c.light_condition = '02'
 
 union all
-select c.casenumber, 'illumination', 'dark_street', 1
-from nj.all_crash c where c.illumination = '3'
+select c.casenumber, 'illumination', 'dusk', 1
+from nj.all_crash c where c.light_condition = '03'
 
 union all
-select c.casenumber, 'illumination', 'dawn_dusk', 1
-from nj.all_crash c where c.illumination in ('4','5')
+select c.casenumber, 'illumination', 'dark_street_lights_off', 1
+from nj.all_crash c where c.light_condition = '04'
 
 union all
-select c.casenumber, 'illumination', 'other_unknown', 1
-from nj.all_crash c where c.illumination in ('6','8','9')
+select c.casenumber, 'illumination', 'dark_no_street_lights', 1
+from nj.all_crash c where c.light_condition = '05'
 
+union all
+select c.casenumber, 'illumination', 'dark_streetlight_continuous', 1
+from nj.all_crash c where c.light_condition = '06'
+
+union all
+select c.casenumber, 'illumination', 'dark_streetlight_spot', 1
+from nj.all_crash c where c.light_condition = '07'
+
+union all
+select c.casenumber, 'illumination', 'other', 1
+from nj.all_crash c where c.light_condition = '99'
 
 /* =========================================================
    MONTH / DAY / HOUR
@@ -180,7 +203,7 @@ from nj.all_crash c
 
 
 /* =========================================================
-   VEHICLE TYPE (COUNTS PER CRASH)
+   VEHICLE TYPE
    ========================================================= */
 union all
 select v.casenumber, 'vehicle', 'automobile', count(*)
@@ -193,56 +216,15 @@ from nj.all_vehicle v where v.veh_type = '02'
 group by v.casenumber
 
 union all
-select v.casenumber, 'vehicle', 'bus', count(*)
-from nj.all_vehicle v where v.veh_type = '03'
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'small_truck', count(*)
-from nj.all_vehicle v where v.veh_type = '04'
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'large_truck', count(*)
-from nj.all_vehicle v where v.veh_type = '05'
-group by v.casenumber
-
-union all
 select v.casenumber, 'vehicle', 'other_motor', count(*)
 from nj.all_vehicle v
-where v.veh_type in ('06','07','08','09','10','11','12','13','14','15','16','17','18','19')
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'bicycle', count(*)
-from nj.all_vehicle v where v.veh_type = '20'
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'pedestrian', count(*)
-from nj.all_vehicle v where v.veh_type = '31'
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'other_nonmotor', count(*)
-from nj.all_vehicle v
-where v.veh_type in ('21','22','23','24','25','32','33','34','35','36','98')
-group by v.casenumber
-
-union all
-select v.casenumber, 'vehicle', 'unknown', count(*)
-from nj.all_vehicle v where v.veh_type = '99'
+where v.veh_type in ('03','04','05','06','07','08','09','10','11','12','13','14','15','16','17','18','19')
 group by v.casenumber
 
 
 /* =========================================================
-   PERSON INJURY SEVERITY (COUNTS PER CRASH)
+   PERSON INJURY SEVERITY
    ========================================================= */
-union all
-select p.casenumber, 'person_injury', 'not_injured', count(*)
-from nj.all_person p where p.inj_severity = '0'
-group by p.casenumber
-
 union all
 select p.casenumber, 'person_injury', 'fatal', count(*)
 from nj.all_person p where p.inj_severity = '1'
@@ -261,11 +243,6 @@ group by p.casenumber
 union all
 select p.casenumber, 'person_injury', 'possible', count(*)
 from nj.all_person p where p.inj_severity = '4'
-group by p.casenumber
-
-union all
-select p.casenumber, 'person_injury', 'injury_unknown', count(*)
-from nj.all_person p where p.inj_severity in ('8','9')
 group by p.casenumber
 
 
