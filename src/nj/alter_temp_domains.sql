c83ad0e18a294bd5c2a722ee0a94fe9e8ab80777
Kris Warner <kris@kdwarn.net>
2026-03-20 23:55:01 -0400

    Put domains in transactions, add & use new one

--- src/nj/alter_temp_domains.sql
+++ src/nj/alter_temp_domains.sql
@@ -7,8 +7,10 @@ begin
     -- Alter domains of temporary tables so that data can be inserted and cleaned later. Start with
     -- most restrictive domains and move to less restrictive until success.
 
-    execute format($q$alter table temp_pedestrian_%s alter dir_of_travel type text_00_04_direction using dir_of_travel::text_00_04_direction$q$, year);
-    execute format($q$alter table temp_vehicle_%s alter dir_of_travel type text_00_04_direction using dir_of_travel::text_00_04_direction$q$, year);
+    -- execute format($q$alter table temp_pedestrian_%s alter dir_of_travel type text_00_04_direction using dir_of_travel::text_00_04_direction$q$, year);
+    -- execute format($q$alter table temp_vehicle_%s alter dir_of_travel type text_00_04_direction using dir_of_travel::text_00_04_direction$q$, year);
+    execute format($q$alter table temp_pedestrian_%s alter dir_of_travel type text_direction1 using dir_of_travel::text_direction1$q$, year);
+    execute format($q$alter table temp_vehicle_%s alter dir_of_travel type text_direction1 using dir_of_travel::text_direction1$q$, year);
     
 end;
 $body$
