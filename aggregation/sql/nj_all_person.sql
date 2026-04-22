ac1ffb12bea927fc5f34f500c19b98005fc4fe5d
brcarney <44352367+brcarney@users.noreply.github.com>
2026-04-22 10:14:01 -0400

    modified crash and person tables to exclude private property crashes in NJ

--- aggregation/sql/nj_all_person.sql
+++ aggregation/sql/nj_all_person.sql
@@ -1,10 +1,10 @@
 drop view if exists nj.all_person cascade;
 create view nj.all_person as
 with occupant as (
-	select casenumber,
-	"year" as crash_year,
-	ncic_code,
-	dept_case_num,
+	select o.casenumber,
+	o."year" as crash_year,
+	o.ncic_code,
+	o.dept_case_num,
 	cast(null as int) as pedestrian_num,
 	veh_num,
 	occupant_num,
@@ -12,7 +12,6 @@ with occupant as (
 	position_in_veh,
 	ejection,
 	case when "age" like '%M%' then '0' else "age" end as "age",
-	"age",
 	sex,
 	location_of_most_severe_injury,
 	type_of_most_severe_injury,
@@ -22,14 +21,16 @@ with occupant as (
 	false as pedestrian,
 	null as contrib_circ1,
 	null as contrib_circ2,
-	safety_equipment_used 
-	null as contrib_circ2
-	from nj.all_occupant),
+	safety_equipment_used
+	from nj.all_occupant o
+	left join nj.all_crash c 
+	on o.casenumber = c.casenumber 
+	where c.road_system <> '09'),
 pedestrian as (
-	select casenumber,
-	"year" as crash_year,
-	ncic_code,
-	dept_case_num,
+	select p.casenumber,
+	p."year" as crash_year,
+	p.ncic_code,
+	p.dept_case_num,
 	pedestrian_num,
 	cast(null as int) as veh_num,
 	cast(null as int) as occupant_num,
@@ -37,7 +38,6 @@ pedestrian as (
 	null as position_in_veh,
 	null as ejection,
 	case when "age" like '%M%' then '0' else "age" end as "age",
-	"age",
 	sex,
 	location_of_most_severe_injury,
 	type_of_most_severe_injury,
@@ -47,11 +47,13 @@ pedestrian as (
 	true as pedestrian,
 	contrib_circ1,
 	contrib_circ2,
-	safety_equipment_used 
-	contrib_circ2
-	from nj.all_pedestrian) 
+	safety_equipment_used
+	from nj.all_pedestrian p 
+	left join nj.all_crash c 
+	on p.casenumber = c.casenumber 
+	where c.road_system <> '09')
 
 
-select * from occupant 
+select * from occupant
 union all 
-select * from pedestrian;
+select * from pedestrian 
