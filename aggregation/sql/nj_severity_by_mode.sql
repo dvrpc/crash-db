d2ffddb2b3bba0e7ae1e6161e1cde6e28845ccf4
brcarney <44352367+brcarney@users.noreply.github.com>
2026-08-25 15:35:28 -0400

    fix: changed partition for vehicle resolution

--- aggregation/sql/nj_severity_by_mode.sql
+++ aggregation/sql/nj_severity_by_mode.sql
@@ -3,6 +3,7 @@ create materialized view nj_report.severity_by_mode as
 with vehicles as (
 select
 	casenumber,
+	veh_num,
 	case 
 		when veh_type = '01' then 'Car/Stationwagon/Minivan'
 		when veh_type = '05' then 'Pickup Truck'
@@ -12,37 +13,44 @@ select
 		when veh_type = '08' then 'Motorcycle'
 		when veh_type in ('12', '14') then 'Other Nonmotor'
 		when veh_type = '13' then 'Bicycle'
+		when veh_type = '11' then 'Moped'
+		when veh_type = '00' then 'Unknown'
 		else null
 	end as vehicle_type
 from
 	nj.all_vehicle),
-vehicles_by_event AS (
-    SELECT
-        casenumber,
-        vehicle_type
-    FROM vehicles
-    WHERE vehicle_type IS NOT NULL
-    GROUP BY casenumber, vehicle_type
+vehicles_by_event as (
+select
+	casenumber,
+	veh_num,
+	vehicle_type
+from
+	vehicles
+where
+	vehicle_type is not null
+group by
+	casenumber,
+	vehicle_type,
+	veh_num
 )
-
 --state
 select
 	'vehicles' as resolution,
 	'state' as geography_resolution,
 	'NJ' as geography,
 	ms.max_severity_level,
-	v.vehicle_type,
+	vbe.vehicle_type,
 	count(*) as total,
-	round(count(*)/ sum(count(*)) over (partition by v.vehicle_type),
+	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type),
 	3) as pct
 from
 	nj.all_crash c
 left join nj_report.max_severity_level ms 
 on
 	c.casenumber = ms.casenumber
-left join vehicles v 
+left join vehicles_by_event vbe
 on
-	c.casenumber = v.casenumber
+	c.casenumber = vbe.casenumber
 group by
 	resolution,
 	geography_resolution,
@@ -74,27 +82,25 @@ group by
 	geography,
 	vehicle_type,
 	max_severity_level
-	
-union all 
-
+union all
 --county
 select
 	'vehicles' as resolution,
 	'county' as geography_resolution,
 	initcap(c.county) as geography,
 	ms.max_severity_level,
-	v.vehicle_type,
+	vbe.vehicle_type,
 	count(*) as total,
-	round(count(*)/ sum(count(*)) over (partition by v.vehicle_type),
+	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type, initcap(c.county)),
 	3) as pct
 from
 	nj.all_crash c
 left join nj_report.max_severity_level ms 
 on
 	c.casenumber = ms.casenumber
-left join vehicles v 
+left join vehicles_by_event vbe
 on
-	c.casenumber = v.casenumber
+	c.casenumber = vbe.casenumber
 group by
 	resolution,
 	geography_resolution,
@@ -110,7 +116,7 @@ select
 	ms.max_severity_level,
 	vbe.vehicle_type,
 	count(*) as total,
-	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type),
+	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type, initcap(c.county)),
 	3) as pct
 from
 	nj.all_crash c
@@ -126,43 +132,16 @@ group by
 	initcap(c.county),
 	vehicle_type,
 	max_severity_level
-
-union all 
-
+union all
 --municipality
 	select
 	'vehicles' as resolution,
 	'municipality' as geography_resolution,
 	initcap(c.municipality) as geography,
 	ms.max_severity_level,
-	v.vehicle_type,
-	count(*) as total,
-	round(count(*)/ sum(count(*)) over (partition by v.vehicle_type),
-	3) as pct
-from
-	nj.all_crash c
-left join nj_report.max_severity_level ms 
-on
-	c.casenumber = ms.casenumber
-left join vehicles v 
-on
-	c.casenumber = v.casenumber
-group by
-	resolution,
-	geography_resolution,
-	initcap(c.municipality),
-	vehicle_type,
-	max_severity_level
-union all 
-
-select
-	'events' as resolution,
-	'municipality' as geography_resolution,
-	initcap(c.municipality) as geography,
-	ms.max_severity_level,
 	vbe.vehicle_type,
 	count(*) as total,
-	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type),
+	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type, initcap(c.municipality)),
 	3) as pct
 from
 	nj.all_crash c
@@ -178,5 +157,35 @@ group by
 	initcap(c.municipality),
 	vehicle_type,
 	max_severity_level
-	
-order by resolution, geography_resolution, geography, vehicle_type, max_severity_level
+union all 
+
+select
+	'events' as resolution,
+	'municipality' as geography_resolution,
+	initcap(c.municipality) as geography,
+	ms.max_severity_level,
+	vbe.vehicle_type,
+	count(*) as total,
+	round(count(*)/ sum(count(*)) over (partition by vbe.vehicle_type, initcap(c.municipality)),
+	3) as pct
+from
+	nj.all_crash c
+left join nj_report.max_severity_level ms 
+on
+	c.casenumber = ms.casenumber
+left join vehicles_by_event vbe 
+on
+	c.casenumber = vbe.casenumber
+group by
+	resolution,
+	geography_resolution,
+	initcap(c.municipality),
+	vehicle_type,
+	max_severity_level
+order by
+	resolution,
+	geography_resolution,
+	geography,
+	vehicle_type,
+    max_severity_level
+    ;
